52487320f68ffd4a1fa769b603ad980919aa9587
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2024-05-28 21:50:17 +0530

    wpi

--- src/run.sh
+++ src/run.sh
@@ -1,2 +1,2 @@
-# echo "./gess3dd/gess301.txt"| bun run src/run_once.js
-echo "./gess3dd/gess301.txt"| bun run src/run_once.js && echo "./gess3dd/gess302.txt"| bun run src/run_once.js  && echo "./gess3dd/gess303.txt"| bun run src/run_once.js && echo "./gess3dd/gess304.txt"| bun run src/run_once.js && echo "./gess3dd/gess305.txt"| bun run src/run_once.js && echo "./gess3dd/gess306.txt"| bun run src/run_once.js  && echo "./gess3dd/gess307.txt"| bun run src/run_once.js && echo "./gess3dd/gess308.txt"| bun run src/run_once.js 
+# echo "./jess3dd/jess101.txt"| bun run src/run_once.js
+echo "./jess3dd/jess101.txt"| bun run src/run_once.js && echo "./jess3dd/jess102.txt"| bun run src/run_once.js  && echo "./jess3dd/jess103.txt"| bun run src/run_once.js && echo "./jess3dd/jess104.txt"| bun run src/run_once.js && echo "./jess3dd/jess105.txt"| bun run src/run_once.js && echo "./jess3dd/jess106.txt"| bun run src/run_once.js  
